cfaa1ee0e95f7a59d9d10ae2afcee9866b4c434c
HP <HP@DESKTOP-B0OK1FL>
2021-01-18 11:18:13 +0700

    Add project files.

---
+++ assingment 4 Employee system/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace assingment_4_Employee_system
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
